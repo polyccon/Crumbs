4192c30a587ed5ba53655bd40d4ebb712b0bfe6e
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-03 15:02:46 +0100

    Changes filepath for config.env relates #9

--- test/database-test/db_connection.test.js
+++ test/database-test/db_connection.test.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const url = require('url');
-require('env2')('./config.env');
+require('env2')('./test/database-test/config.env');
 
 if (!process.env.DB_URL) throw new Error('Environment variable DB_URL must be set');
 
