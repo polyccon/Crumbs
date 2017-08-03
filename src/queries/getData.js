53dd5be7ffb3b98654f98182afdd45fb502ab7a9
RogeredBacon <jinglis12@googlemail.com>
2017-08-03 13:00:57 +0100

    Started getdata.js #27

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,7 +1,7 @@
 const dbConnection = require('../database/db_connection.js');
 
 const getData = (cb) => {
-  dbConnection.query(`SELECT * FROM users`, (err, res) => {
+  dbConnection.query(`SELECT * FROM biscuits`, (err, res) => {
     if (err) {
       cb(err);
     } else {
